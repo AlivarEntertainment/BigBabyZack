45ec514d40ed90f4c6c2ab1ea5bfed14364ce2d0
AlivarEntertainment <AlivarEnt@yandex.ru>
2024-10-05 09:56:13 +0500

    EnemyController
    Sort smth in folders and add enemy AI

--- Assets/EnemyPapka/Enemy.cs
+++ Assets/EnemyPapka/Enemy.cs
@@ -6,10 +6,83 @@ public class Enemy : MonoBehaviour
 {
     public int maxHeath = 5;
     public int currentHealth;
+    [Header("Patroler")]
+    public float speed;
+    public int positionOfPatrol;
+    public Transform point;
+    bool moveingRight;
+
+    Transform player;
+    public float StoppingDistance;
+    bool chill = false;
+    bool angry = false;
+    bool goBack = false;
     //121212
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         currentHealth = maxHeath;
+    }
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, point.position) < positionOfPatrol && angry == false)
+        {
+            chill = true;
+        }
+        if(Vector2.Distance(transform.position, player.position) < StoppingDistance)
+        {
+           angry = true;
+           chill = false;
+           goBack = false;
+        }
+        if(Vector2.Distance(transform.position, player.position) > StoppingDistance)
+        {
+            goBack = true;
+            angry = false;
+        }
+        if(chill == true)
+        {
+            Chill();
+        }
+        else if(angry == true)
+        {
+            Angry();
+        }
+        else if(goBack == true)
+        {
+            GoBack();
+        }
+    }
+    void Chill()
+    {   
+        speed = 2;
+        if(transform.position.x > point.position.x + positionOfPatrol)
+        {
+            moveingRight = false;
+        }
+        else if(transform.position.x < point.position.x - positionOfPatrol)
+        {
+            moveingRight = true;
+        }
+        if(moveingRight)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+
+        }
+    }
+    void Angry()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        speed = 3;
+    }
+    void GoBack()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
     }
     public void TakeDamage(int Damage)
     {
