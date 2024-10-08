bbf36612e8cc0019c45e4801672cb628807454a9
Mel0Man74 <levmerkulev@gmail.com>
2024-10-09 00:31:30 +0500

    LevelDesign + change scene script

---
+++ Assets/Scripts/Change.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Change : MonoBehaviour
+{
+    public int sceneNumber;
+    public void OnTriggerEnter2D(Collider2D otherPlayer)
+    {
+        if (otherPlayer.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
+    }
+}
