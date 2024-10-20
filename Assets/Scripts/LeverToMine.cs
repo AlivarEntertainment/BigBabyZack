5430530ec99839a585c96f95ee1be2c93dc8aad7
AlivarEntertainment <AlivarEnt@yandex.ru>
2024-10-21 00:37:20 +0500

    SecondLevel work

---
+++ Assets/Scripts/LeverToMine.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverToMine : MonoBehaviour
+{   
+    public bool UsedLever;
+    public GameObject Blocker;
+    public Rigidbody2D MinecartRB;
+    public void OnTriggerStay2D(Collider2D PlayerCol)
+    {
+        if(PlayerCol.gameObject.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                UsedLever = true;
+                Blocker.SetActive(false);
+                MinecartRB.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+}
