b3c95ed13f33636925447cc5dbbbefe0c3a3c2a5
Mel0Man74 <levmerkulev@gmail.com>
2024-11-25 18:35:22 +0500

    PlateOnFourLevel

--- Assets/Scripts/OpenGates.cs
+++ Assets/Scripts/OpenGates.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OpenGates : MonoBehaviour
 {
     public Animator GatesAnimator;
+    public Animator PlateAnimator;
     [SerializeField]private Animator PressE;
 
     public void OnTriggerStay2D(Collider2D other)
@@ -15,6 +16,7 @@ public class OpenGates : MonoBehaviour
             if (Input.GetKey(KeyCode.E))
             {
                 GatesAnimator.SetTrigger("GatesOpen");
+                PlateAnimator.SetTrigger("OpenBlock");
             }
         }
     }
