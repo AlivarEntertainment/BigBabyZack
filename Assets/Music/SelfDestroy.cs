dbdfa1895146f5c5996c17b391d2bdad0bd675de
AlivarEnt <AlivarEnt@yandex.ru>
2024-12-12 23:36:51 +0500

    Reapply "Changes"
    This reverts commit c9a47b95280af491b0c564250c115cbaf828fe29.

--- Assets/Music/SelfDestroy.cs
+++ Assets/Music/SelfDestroy.cs
@@ -6,17 +6,38 @@ public class SelfDestroy : MonoBehaviour
 {
     public AudioClip[] audioClips;
     public AudioSource audioSource;
+<<<<<<< HEAD
 
+=======
+    public bool IsAlreadyStarted = false;
+    public void Awake()
+    {
+        IsAlreadyStarted = true;
+    }
+>>>>>>> b1046b8 (Roof)
     public void OnLevelWasLoaded()
     {
         int scene = SceneManager.GetActiveScene().buildIndex;
         Debug.Log(scene);
+<<<<<<< HEAD
         if (scene == 2)
+=======
+        if (scene == 0 || scene == 25 || scene == 18)
+        {
+            audioSource.clip = audioClips[3];
+            audioSource.Play();
+        }
+        if (scene == 1)
+>>>>>>> b1046b8 (Roof)
         {
             audioSource.clip = audioClips[0];
             audioSource.Play();
         }
+<<<<<<< HEAD
         if (scene == 3)
+=======
+        if (scene >= 3 && audioSource.clip != audioClips[1] && scene <= 9)
+>>>>>>> b1046b8 (Roof)
         {
             audioSource.clip = audioClips[1];
             audioSource.Play();
